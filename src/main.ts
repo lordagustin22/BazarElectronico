c5a4f834195545fdd8a96d289d752c3df6929fc0
lordagustin22 <niconecrofilo589@gmail.com>
2023-12-11 12:38:50 -0300

    Prueba de branch testing

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+// Prueba para hacer un cambio en el commit
+// Y luego testear de subir esto solo al branch "testing"
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
