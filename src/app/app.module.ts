355d6b1b62371b4c1f468cc677cf9c2a8137f12e
lordagustin22 <niconecrofilo589@gmail.com>
2023-12-18 03:31:31 -0300

    Added toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { LayoutModule } from './components/layout/layout.module';
 import { HomeModule } from './home/home.module';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { EditDeleteComponent } from './components/edit-delete/edit-delete.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, EditDeleteComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -17,6 +19,11 @@ import { AppComponent } from './app.component';
     HomeModule,
     LayoutModule,
     HttpClientModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
