dbbb8bb28711f87e76c94c9ca51df683c16d2d0c
lordagustin22 <niconecrofilo589@gmail.com>
2023-12-19 23:14:13 -0300

    El navbar tiene la barra de busqueda

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SearchbarComponent } from '../searchbar/searchbar.component';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
+  standalone: true,
+  imports: [SearchbarComponent, RouterModule],
 })
 export class NavbarComponent {
   title = 'ElectroBazar';
