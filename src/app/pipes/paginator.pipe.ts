bcad7b00a57c190709df840c5deec246bf6e08a6
lordagustin22 <niconecrofilo589@gmail.com>
2023-12-21 19:28:28 -0300

    Paginador solo muestra la pagina 1, pero funciona con observables

--- src/app/pipes/paginator.pipe.ts
+++ src/app/pipes/paginator.pipe.ts
@@ -1,24 +1,28 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Product } from '../interfaces/product';
 
 @Pipe({
   name: 'paginator',
 })
 export class PaginatorPipe implements PipeTransform {
   transform(
-    array: any[],
-    pageSize: number | string,
-    pageNumber: number
-  ): any[] {
-    if (!array.length) return [];
+    array$: Observable<Product[]>,
+    pageSize: number | string = 10,
+    pageNumber: number = 1
+  ): Observable<Product[]> {
+    return array$.pipe(
+      map((array) => {
+        if (!array.length) throw new Error('No hay productos');
 
-    if (pageSize === 'all') {
-      return array;
-    }
+        if (pageSize === 'all') return array;
 
-    pageSize = pageSize || 10;
-    pageNumber = pageNumber || 1;
-    --pageNumber;
-    // @ts-ignore
-    return array.slice(pageNumber * pageSize, (pageNumber + 1) * pageSize);
+        pageSize = pageSize || 10;
+        pageNumber = pageNumber || 1;
+        --pageNumber;
+        // @ts-ignore
+        return array.slice(pageNumber * pageSize, (pageNumber + 1) * pageSize);
+      })
+    );
   }
 }
