339556c6482c63273d3e8f90d7430d34591feceb
lordagustin22 <niconecrofilo589@gmail.com>
2023-12-26 19:18:47 -0300

    Creada la pagina de detalles

--- src/app/home/pages/detalles/detalles.component.ts
+++ src/app/home/pages/detalles/detalles.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/interfaces/product';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-detalles',
   templateUrl: './detalles.component.html',
-  styleUrls: ['./detalles.component.scss']
+  styleUrls: ['./detalles.component.scss'],
 })
-export class DetallesComponent {
+export class DetallesComponent implements OnInit {
+  productoService: ProductoService = inject(ProductoService);
+  private id: number;
+  product: Product | null = null;
 
+  constructor(private route: ActivatedRoute) {
+    this.id = Number(route.snapshot.paramMap.get('id'));
+  }
+
+  ngOnInit(): void {
+    this.getProduct(this.id);
+  }
+
+  getProduct(id: number) {
+    this.productoService.getProduct(id).subscribe((p) => {
+      this.product = p;
+      console.log(this.product);
+    });
+  }
 }
