dfc5ef65194f617d68301b6aa5141e784d031c3e
lordagustin22 <niconecrofilo589@gmail.com>
2023-12-31 16:50:09 -0300

    Add firebase support

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,34 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+/*
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLbAnbZwyNuzpDVxf4k01Cz_ykD5bWfzo",
+  authDomain: "bazarelectronico-347d2.firebaseapp.com",
+  projectId: "bazarelectronico-347d2",
+  storageBucket: "bazarelectronico-347d2.appspot.com",
+  messagingSenderId: "812323407064",
+  appId: "1:812323407064:web:042203b640dc91d69f43ad"
+},
+    production: false
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+*/
+
+/*
 export const environment = {
   production: false,
   endpoint: 'http://localhost:3000/',
 };
+*/
 
 /*
  * For easier debugging in development mode, you can import the following file
